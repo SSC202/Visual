e5d3fdcca6106f3a87fa5729f1a7a957cae07466
SSC202 <e22750706642022@163.com>
2023-09-20 21:41:00 +0800

    Harris 角点检测

--- OpenCV-Python/Code/8_Feature_detection/harris.py
+++ OpenCV-Python/Code/8_Feature_detection/harris.py
@@ -10,10 +10,38 @@ while cap.isOpened() == True:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = np.float32(gray)
 
+        # 第一步：角点检测
         dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+        # 第二步：将检测到的角点进行膨胀操作
         dst = cv2.dilate(dst, None)
 
-        frame[dst > 0.01*(dst.max())] = [0, 0, 255]
+        # frame[dst > 0.01*(dst.max())] = [0, 0, 255]
+        # 第三步：二值化角点图像，使用0.01倍最大值进行过滤
+        res, dst = cv2.threshold(dst, (0.01*dst.max()), 255, cv2.THRESH_BINARY)
+        dst = np.uint8(dst)
+
+        # 第四步：取连通角点的质心进行修正
+        res, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+
+        # 第五步：定义停止条件(迭代最近点算法的终止条件)
+        """
+            cv2.TERM_CRITERIA_EPS 指定收敛准则的epsilon值,即允许两点之间存在的最大差异。
+            cv2.TERM_CRITERIA_MAX_ITER 指定算法允许的最大迭代次数,以便其收敛。如果在这么多的迭代次数内算法没有收敛,它将停止并返回当前的最佳解决方案。
+            100 指定算法允许的最大迭代次数以使其收敛。在这种情况下,如果算法在100次迭代后仍未收敛,它将停止。
+            0.01 指定收敛准则的容差级别。这意味着如果两个点之间的差异小于或等于0.01,它们将被视为相同。
+        """
+        criteria = (cv2.TERM_CRITERIA_EPS +
+                    cv2.TERM_CRITERIA_MAX_ITER, 100, 0.01)
+
+        # 第六步：
+        corners = cv2.cornerSubPix(gray, np.float32(centroids),
+                                   (5, 5), (-1, -1), criteria)
+        result = np.hstack((centroids, corners))
+        
+        result = np.int0(result)
+        frame[result[:, 1], result[:, 0]] = [0, 0, 255]
+        frame[result[:, 3], result[:, 2]] = [0, 255, 0]
+
         cv2.imshow('res', frame)
         key = cv2.waitKey(1)
         if key == 27:
